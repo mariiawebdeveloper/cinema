b3f257e92557e3ad00111bf63d0cf54f229d4311
MariiaMarchenko <96719405+MariiaMarchenko@users.noreply.github.com>
2023-06-05 09:27:31 +0200

    working api

--- pages/api/movies.js
+++ pages/api/movies.js
@@ -1,14 +1,22 @@
-import { PrismaClient } from "@prisma/client";
+import {PrismaClient} from "@prisma/client";
 
-export default async function handler(
-    req,
-    res
-) {
+export default async function handler(req, res) {
     const prisma = new PrismaClient();
-    console.log(prisma.movie)
-    const movieList =  prisma.movie.findMany();
 
+    await prisma
+        .$connect()
+        .then(() => prisma.movie.findMany({
+            where: {
+                movie_id: 1,
 
-    res.status(200).json({ movieList });
+            },}))
+        .then(movieList => {
+            res.status(200).json({result: movieList});
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(500).json({error: "Server error"});
 
+        })
+        .finally(prisma.$disconnect)
 }
